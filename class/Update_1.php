04ad506617e32abcbe108539ae0f93d43897a2cb
trueeric <ikpassat@gmail.com>
2021-10-27 14:36:07 +0800

    6-4-3 安裝模組時自動完成群組權限設定

--- class/Update_1.php
+++ class/Update_1.php
@@ -2,26 +2,10 @@
 // 如「模組目錄」= signup，則「首字大寫模組目錄」= Signup
 // 如「資料表名」= actions，則「模組物件」= Actions
 
-namespace XoopsModules\Eric_signup;
+namespace XoopsModules\首字大寫模組目錄;
 
 class Update
 {
-    // 建立群組
-    public static function mk_group($name = "")
-    {
-        global $xoopsDB;
-        $sql           = "select groupid from " . $xoopsDB->prefix("groups") . " where `name`='$name'";
-        $result        = $xoopsDB->query($sql) or Utility::web_error($sql, __FILE__, __LINE__);
-        list($groupid) = $xoopsDB->fetchRow($result);
-        if (empty($groupid)) {
-            $sql = "insert into " . $xoopsDB->prefix("groups") . " (`name`) values('{$name}')";
-            $xoopsDB->queryF($sql) or Utility::web_error($sql, __FILE__, __LINE__);
-            //取得最後新增資料的流水編號
-            $groupid = $xoopsDB->getInsertId();
-        }
-        return $groupid;
-    }
-
     // 進行某些檢查
     public static function chk_1()
     {
--- include/onInstall.php
+++ include/onInstall.php
@@ -2,7 +2,9 @@
 // 如「eric_signup」= signup，則「首字大寫eric_signup」= Signup
 // 如「資料表名」= actions，則「模組物件」= Actions
 
+use XoopsModules\Eric_signup\Update;
 use XoopsModules\Tadtools\Utility;
+
 if (!class_exists('XoopsModules\Tadtools\Utility')) {
     require XOOPS_ROOT_PATH . '/modules/tadtools/preloads/autoloader.php';
 }
@@ -25,5 +27,13 @@ function xoops_module_install_eric_signup(XoopsModule $module)
     Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/eric_signup/image");
     Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/eric_signup/image/.thumbs");
 
+    $groupid      = Update::mk_group("活動報名管理");
+    $perm_handler = xoops_getHandler('groupperm');
+    $perm         = $perm_handler->create();
+    $perm->setVar('gperm_groupid', $groupid);
+    $perm->setVar('gperm_itemid', 1);
+    $perm->setVar('gperm_name', $module->dirname()); //一般為模組目錄名稱
+    $perm->setVar('gperm_modid', $module->mid());
+    $perm_handler->insert($perm);
     return true;
 }

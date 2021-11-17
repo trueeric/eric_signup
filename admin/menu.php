7fed7e9de79c414b5ee9f2c3dc4bf0e8b90b78ee
trueeric <ikpassat@gmail.com>
2021-11-17 16:51:00 +0800

    9-3-3-1 admin\menu.php 套用語系

--- admin/menu.php
+++ admin/menu.php
@@ -1,5 +1,5 @@
 <?php
 $adminmenu[] = ['title' => _MI_TAD_ADMIN_HOME, 'link' => 'admin/index.php', 'icon' => 'images/admin/home.png'];
-$adminmenu[] = ['title' => _MI_ERIC_SIGNUP_ADMENU1, 'link' => 'admin/main.php', 'icon' => 'images/admin/button.png'];
-$adminmenu[] = ['title' => _MI_ERIC_SIGNUP_ADMENU2, 'link' => 'admin/power.php', 'icon' => 'images/admin/button.png'];
+$adminmenu[] = ['title' => _MI_ERIC_ACTIONS_MANAGERMENT, 'link' => 'admin/main.php', 'icon' => 'images/admin/button.png'];
+$adminmenu[] = ['title' => _MI_ERIC_SIGNUP_ACTION_PERMISSION_SETTING, 'link' => 'admin/power.php', 'icon' => 'images/admin/button.png'];
 $adminmenu[] = ['title' => _MI_TAD_ADMIN_ABOUT, 'link' => 'admin/about.php', 'icon' => 'images/admin/about.png'];
--- language/tchinese_utf8/modinfo.php
+++ language/tchinese_utf8/modinfo.php
@@ -1,14 +1,13 @@
 <?php
 xoops_loadLanguage('modinfo_common', 'tadtools');
 
-define("_MI_ERIC_SIGNUP_ADMENU1", "活動管理");
-define("_MI_ERIC_SIGNUP_ADMENU1_DESC", "後台活動管理頁");
-
-define("_MI_ERIC_SIGNUP_ADMENU2", "權限管理");
-define("_MI_ERIC_SIGNUP_ADMENU2_DESC", "後台權限管理");
-
+// xoops_version.php
 define("_MI_ERIC_SIGNUP_SHOW_NUMBER", "每頁顯示活動數量");
 define("_MI_ERIC_SIGNUP_SHOW_NUMBER_DESC", "每頁顯示活動的數量，作為分頁依據");
 
 define("_MI_ERIC_SIGNUP_ONLY_ENABLE", "是否只顯可報名活動");
 define("_MI_ERIC_SIGNUP_ONLY_ENABLE_DESC", "若是只會顯示可報名活動，否則無法報名活動也會列出");
+
+// admin\menu
+define("_MI_ERIC_ACTIONS_MANAGERMENT", "活動管理");
+define('_MI_ERIC_SIGNUP_ACTION_PERMISSION_SETTING', '權限設定');
